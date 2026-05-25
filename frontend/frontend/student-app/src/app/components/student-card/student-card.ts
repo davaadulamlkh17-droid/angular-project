7410127e43fc18583787c57da4567e61282afba8
khuslen <batnyamhuslen@gmail.com>
2026-05-25 12:58:33 +0800

    frontend fix

--- frontend/frontend/student-app/src/app/components/student-card/student-card.ts
+++ frontend/frontend/student-app/src/app/components/student-card/student-card.ts
@@ -1,6 +1,6 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Student } from '../../../models/student.model';
+import { Student } from '../../models/student.model';
 
 @Component({
   selector: 'app-student-card',
